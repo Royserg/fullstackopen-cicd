1895ea6c28e56a11e241cff4239c20a6582e68cb
Royserg <staniojakub@gmail.com>
2020-12-12 15:52:25 +0100

    Add test checking navigation to pokemon details

--- cypress/integration/e2e/Pokedex.cy.js
+++ cypress/integration/e2e/Pokedex.cy.js
@@ -1,9 +1,22 @@
+/// <reference types="cypress" />
+
 describe('Pokedex', () => {
-  it('front page can be opened', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  it('should open the front page without errors', () => {
     cy.contains('ivysaur');
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     );
+  });
+
+  it('should navigate to pokemon page', () => {
+    cy.contains('charizard');
+    cy.get('[href="/pokemon/charizard"]').click();
+
+    cy.url().should('contain', '/pokemon/charizard');
+    cy.contains('blaze');
   });
 });
